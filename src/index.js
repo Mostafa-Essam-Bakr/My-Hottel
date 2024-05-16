186a2ed7e8c2fd20ab687999582662dc4692a5e0
Your Name <you@example.com>
2024-05-16 15:25:44 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import "./Style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
